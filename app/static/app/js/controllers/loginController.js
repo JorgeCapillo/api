dab24e80788ef467afdbfb0dd9b82d554bd3ef89
hcastilla <hernancastillaq@gmail.com>
2015-10-09 21:53:28 -0500

    implementacion de login

--- app/static/app/js/controllers/loginController.js
+++ app/static/app/js/controllers/loginController.js
@@ -1,13 +1,13 @@
 app.controller('loginController',function($scope,$http){
     $scope.user = {};
     $scope.login = function(){
-    $scope.user.password = $scope.password;
-    $scope.user.email = $scope.email;
-    $http.post('http://localhost:8000/usuarios/login',$scope.user).success(function(msg){
+        $scope.user.password = $scope.password;
+        $scope.user.email = $scope.email;
+        $http.post('http://localhost:8000/usuarios/login',$scope.user).success(function(msg){
             if(msg == "Logged User"){
-                location.reload()
+                location.reload();
             }else{
-                alert(msg)
+                alert(msg);
             }
         })
     }
